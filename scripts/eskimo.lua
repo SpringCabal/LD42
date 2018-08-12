d13c627ca2aa4feb52d7c4dac231a7b3dc1a8e8c
Anarchid <anarchid@mail.ru>
2018-08-13 02:18:28 +0300

    add eskimo death by shooting

--- scripts/eskimo.lua
+++ scripts/eskimo.lua
@@ -42,7 +42,7 @@ Animations['guy'] = {
 Animations['idle'] = VFS.Include("Scripts/animations/idle.lua", scriptEnv)
 Animations['walk'] = VFS.Include("Scripts/animations/walk.lua", scriptEnv)
 Animations['throw'] = VFS.Include("Scripts/animations/throw.lua", scriptEnv)
-
+Animations['death_shot'] = VFS.Include("Scripts/animations/death_shot.lua", scriptEnv)
 
 function constructSkeleton(unit, piece, offset)
     if (offset == nil) then
@@ -165,4 +165,12 @@ function script.AimWeapon()
     Signal(SIG_AIM)
     SetSignalMask(SIG_AIM)
     return Throw()
+end
+
+function script.Killed(recentDamage, _)
+	Signal(SIG_AIM);
+	Signal(SIG_WALK);
+	Signal(SIG_IDLE);
+	PlayAnimation('death_shot');
+    return 1;
 end
