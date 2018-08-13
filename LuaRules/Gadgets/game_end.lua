5f138921221868e7b89356c34905416b7b5cf2e7
Gajo Petrovic <gajo01@gmail.com>
2018-08-13 18:05:57 +0900

    game_end rules

--- LuaRules/Gadgets/game_end.lua
+++ LuaRules/Gadgets/game_end.lua
@@ -27,16 +27,35 @@ end
 local initializeFrame = 0
 
 function gadget:Initialize()
+	Spring.SetGameRulesParam("gameEnd", nil)
 	initializeFrame = Spring.GetGameFrame() or 0
 end
 
+local eskimoDefID = UnitDefNames["eskimo"].id
+function EndCheck()
+	local isLoss = true
+	for _, unitID in ipairs(Spring.GetAllUnits()) do
+		if eskimoDefID == Spring.GetUnitDefID(unitID) then
+			isLoss = false
+		end
+	end
+	if isLoss then
+		return "loss"
+	end
+
+	-- There is no victory, only suffering
+end
+
 function gadget:GameFrame(frame)
+	if Spring.GetGameRulesParam("sb_gameMode") == "dev" then
+		return
+	end
 	if frame > initializeFrame + 2 then
-		local carrotCount = Spring.GetGameRulesParam("carrot_count")
-		
-        -- We're doing widget-only game overs which makes restarts easier
--- 		if carrotCount <= 0 then
--- 			Spring.GameOver({})
--- 		end
+		local endCheck = EndCheck()
+		if endCheck == "victory" then
+			Spring.SetGameRulesParam("gameEnd", "victory")
+		elseif endCheck == "loss" then
+			Spring.SetGameRulesParam("gameEnd", "loss")
+		end
 	end
 end
